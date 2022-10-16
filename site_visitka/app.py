7991ea6910e4623d640fbc85a6b1b935d4bbb4b4
Zhaniya110 <90666680+Zhaniya110@users.noreply.github.com>
2022-10-16 17:38:34 +0600

    Add files via upload

---
+++ site_visitka/app.py
@@ -0,0 +1,74 @@
+# import main Flask class and request object
+from flask import Flask, request
+
+# create the Flask app
+app = Flask(__name__)
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+
+    return '''
+              <h1>The language value is: {}</h1>
+        '''.format(language,)
+              
+# allow both GET and POST requests
+# allow both GET and POST requests
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    # handle the POST request
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form.get('framework')
+        return '''
+                  <h1>The  NFT value is: {}</h1>
+             '''.format(language )
+
+    # otherwise handle the GET request
+    return '''
+           <form method="POST">
+               <div><label>NFT Adress: <input type="text" name="language"></label></div>
+              
+               <input type="submit" value="Submit">
+           </form>'''
+# GET requests will be blocked
+# GET requests will be blocked
+@app.route('/json-example', methods=['POST'])
+def json_example():
+    request_data = request.get_json()
+
+    language = None
+    framework = None
+    python_version = None
+    example = None
+    boolean_test = None
+
+    if request_data:
+        if 'language' in request_data:
+            language = request_data['language']
+
+        if 'framework' in request_data:
+            framework = request_data['framework']
+
+        if 'version_info' in request_data:
+            if 'python' in request_data['version_info']:
+                python_version = request_data['version_info']['python']
+
+        if 'examples' in request_data:
+            if (type(request_data['examples']) == list) and (len(request_data['examples']) > 0):
+                example = request_data['examples'][0]
+
+        if 'boolean_test' in request_data:
+            boolean_test = request_data['boolean_test']
+
+    return '''
+           The language value is: {}
+           The framework value is: {}
+           The Python version is: {}
+           The item at index 0 in the example list is: {}
+           The boolean value is: {}'''.format(language, framework, python_version, example, boolean_test)
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
